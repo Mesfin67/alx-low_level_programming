07bb0e4bd50af1b327f9280845f63fd4b1a8c725
Mesfin67 <mesfinbebulo@gmail.com>
2023-09-11 09:58:50 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - the main function
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
