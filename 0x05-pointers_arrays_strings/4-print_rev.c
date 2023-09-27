dd534f6e26be95e04edb23ebd7bdc3dade62300e
Mesfin67 <mesfinbebulo@gmail.com>
2023-09-26 19:51:40 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,6 +3,21 @@
  * print_rev -prints a string in reverse
  * @s: string to print
  */
+
+/*
+1. Declare a function that takes a string as an argument.
+2. Declare an integer variable to hold the length of the string.
+3. Declare an integer variable to hold the index of the string.
+4. Set the length variable to the length of the string.
+5. Set the index variable to the length of the string minus 1.
+6. Loop through the string starting at the last character and ending at the first character.
+7. Print the character at the current index.
+8. Decrement the index variable.
+9. Print a new line.
+*/
+/*
+Time Complexity: O(n)
+*/
 void print_rev(char *s)
 {
 	int len = 0, index = 0;
@@ -14,11 +29,25 @@ void print_rev(char *s)
 
 	_putchar('\n');
 }
+
 /**
  *_strlen - returns the length of a string
  * @s: string
  *Return: returns length
  */
+
+/*
+1. Declare a function that returns an integer and takes a pointer to a character as an argument.
+2. Declare an integer variable count and initialize it to 0.
+3. Declare an integer variable inc and initialize it to 0.
+4. Use a for loop to iterate through the string s.
+5. Increment inc by 1.
+6. Return inc.
+*/
+
+/*
+Time Complexity: O(n)
+*/
 int _strlen(char *s)
 {
 	int count, inc;
@@ -28,4 +57,5 @@ int _strlen(char *s)
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
 
+	return (inc);
 }
