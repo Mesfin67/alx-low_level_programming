621739698734a37aeb9e29f1bfcedf2d30591dc6
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-13 13:38:20 -0700

    Added a function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,11 @@
+#include "main.h"
+
 /**
  * malloc_checked - allocates memory using malloc, exits with status code 98 if it fails
+ *
+ * This function allocates a block of memory `memory_size` bytes in size using malloc.
+ * If the memory allocation fails, the function exits the program with status code 98.
+ *
  * @memory_size: size of the memory block to be allocated in bytes
  *
  * Return: a pointer to the allocated memory block, or NULL if the allocation fails
