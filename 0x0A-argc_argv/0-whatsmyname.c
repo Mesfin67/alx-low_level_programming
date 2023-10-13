8101aae77f51ec5b15b8fdb6c2bdad528bff0ff3
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-13 11:53:06 -0700

    Add program that prints its name, with dynamic name resolution.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - prints the name of the program to the console
- *
- * This function prints the name of the program to the console. It takes the
- * number of command-line arguments and the array of command-line arguments as
- * arguments, but does not use them.
+ * main - prints the name of the program, followed by a new line
  *
  * @argc: number of command-line arguments
  * @argv: array of command-line arguments
  *
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
     printf("%s\n", argv[0]);
 
-    return (0);
+    return 0;
 }
 
