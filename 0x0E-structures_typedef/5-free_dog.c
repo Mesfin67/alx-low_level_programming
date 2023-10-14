a6118a0e8dcadd306e296861ad6e4053d8db708e
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-13 22:15:38 -0700

    Added a function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * free_dog - Frees all the resources allocated to a dog struct.
+ *
+ * @d: The dog struct to be freed.
+ */
+void free_dog(dog_t *d)
+{
+    if (d == NULL)
+    {
+        return;
+    }
+
+    free(d->name);
+    free(d->owner);
+    free(d);
+}
+
