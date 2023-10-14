d367b5e53fd08e277214d18fd0b45eee845d5c48
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-13 21:52:54 -0700

    Defined a new type struct dog with the following elements:
    name, type = char *
    age, type = float

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,40 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @name: the dog's name
+ * @age: the dog's age in years
+ * @owner: the dog's owner's name
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+/**
+ * @typedef dog_t: a typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * init_dog - initializes a dog struct
+ *
+ * This function initializes a dog struct with the given name, age, and owner.
+ *
+ * @d: a pointer to the dog struct to be initialized
+ * @name: the dog's name
+ * @age: the dog's age in years
+ * @owner: the dog's owner's name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
+#endif
