30b07969651c632ace8c879680842ed5049f61f9
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-20 02:56:30 -0700

    Added a function that prints a number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,21 +2,20 @@
 
 /**
  * print_number - prints an integer to the console
- *
  * @n: the integer to be printed
  */
-void print_number(int n) {
-  unsigned int number = n;
+void print_number(int n)
+{
+	unsigned int number = n;
 
-  if (n < 0) {
-    _putchar('-');
-    number = -number;
-  }
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
 
-  if (number / 10 > 0) {
-    print_number(number / 10);
-  }
+	if ((number / 10) > 0)
+		print_number(number / 10);
 
-  _putchar((number % 10) + '0');
+	_putchar((number % 10) + '0');
 }
-
