ef8c809df7858b89ca50907e6d5a36babb0129ca
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-22 09:09:29 -0700

    Added a program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
-* main - prints all command-line arguments to the console
-*
-* @num_args: number of command-line arguments
-* @args: array of command-line arguments
-*
-* Return: 0
-*/
+ * main - prints all command-line arguments to the console
+ * @num_args: number of command-line arguments
+ * @args: array of command-line arguments
+ * Return: 0
+ */
 int main(int num_args, char *args[])
 {
-    int i;
+	int i;
 
-    for (i = 0; i < num_args; i++) {
-        printf("%s\n", args[i]);
-    }
+	for (i = 0; i < num_args; i++)
+	{
+		printf("%s\n", args[i]);
+	}
 
-    return (0);
+	return (0);
 }
 
