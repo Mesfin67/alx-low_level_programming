3294d1b29248bf6c7b4b6e3ac622bc6f71abb4d7
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-23 04:54:33 -0700

    program that prints the name of the file it was compiled from

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,17 +2,14 @@
 
 /**
  * main - prints the name of the source file it was compiled from
- *
  * This function uses the `__FILE__` macro to get the name of the
  * source file it was compiled from. It then prints the name of the
  * source file to the console.
- *
  * Return: nothing to be returned.
  */
 
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
-
