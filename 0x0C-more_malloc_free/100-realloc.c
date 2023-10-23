9c52b765b89c4cd78d1abbc462d62f2024da33ef
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-23 07:34:29 -0700

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,36 +1,43 @@
+#include "main.h"
+
 /**
- * realloc_memory - reallocates a memory block
- *
+ * _realloc - reallocates a memory block
  * @ptr: pointer to the memory previously allocated with a call to malloc
  * @old_size: size of ptr
  * @new_size: size of the new memory to be allocated
  *
- * Return: pointer to the address of the new memory block, or NULL on failure
+ * Return: pointer to the address of the new memory block
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    void *temp_block;
-    unsigned int i;
+	void *temp_block;
+	unsigned int i;
 
-    if (ptr == NULL) {
-        return malloc(new_size);
-    } else if (new_size == old_size) {
-        return ptr;
-    } else if (new_size == 0 && ptr != NULL) {
-        free(ptr);
-        return NULL;
-    } else {
-        temp_block = malloc(new_size);
-        if (temp_block != NULL) {
-            for (i = 0; i < min(old_size, new_size); i++) {
-                *((char *)temp_block + i) = *((char *)ptr + i);
-            }
-            free(ptr);
-            return temp_block;
-        } else {
-            return NULL;
-        }
-    }
+	if (ptr == NULL)
+	{
+		temp_block = malloc(new_size);
+		return (temp_block);
+	}
+	else if (new_size == old_size)
+		return (ptr);
+
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		temp_block = malloc(new_size);
+		if (temp_block != NULL)
+		{
+			for (i = 0; i < min(old_size, new_size); i++)
+				*((char *)temp_block + i) = *((char *)ptr + i);
+			free(ptr);
+			return (temp_block);
+		}
+		else
+			return (NULL);
+	}
 }
-
