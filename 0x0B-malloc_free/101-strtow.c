76de9b1e222078c46458e3a66ec4531b2be77f1e
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-23 20:54:08 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,38 +1,100 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_tab - Prints an array of string
- * @tab: The array to print
+ *strtow - splits a stirng into words
+ *@str: string to be splitted
  *
- * Return: nothing
+ *Return: pointer to the array of splitted words
  */
-void print_tab(char **tab)
-{
-	int i;
 
-	for (i = 0; tab[i] != NULL; ++i)
+char **strtow(char *str)
+{
+	char **split;
+	int i, j = 0, temp = 0, size = 0, words = num_words(str);
+
+	if (words == 0)
+		return (NULL);
+	split = (char **)malloc(sizeof(char *) * (words + 1));
+	if (split != NULL)
 	{
-		printf("%s\n", tab[i]);
+		for (i = 0; i <= len(str) && words; i++)
+		{
+			if ((str[i] != ' ') && (str[i] != '\0'))
+				size++;
+			else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
+			{
+				split[j] = (char *)malloc(sizeof(char) * size + 1);
+				if (split[j] != NULL)
+				{
+					while (temp < size)
+					{
+						split[j][temp] = str[(i - size) + temp];
+						temp++;
+					}
+					split[j][temp] = '\0';
+					size = temp = 0;
+					j++;
+				}
+				else
+				{
+					while (j-- >= 0)
+						free(split[j]);
+					free(split);
+					return (NULL);
+				}
+			}
+		}
+		split[words] = NULL;
+		return (split);
 	}
+	else
+		return (NULL);
 }
 
 /**
- * main - check the code for ALX School students.
+ * num_words - counts the number of words in str
+ *@str: string to be used
  *
- * Return: 1 if an error occurred, 0 otherwise
+ *Return: number of words
  */
-int main(void)
+int num_words(char *str)
 {
-	char **tab;
+	int i = 0, words = 0;
 
-	tab = strtow("      ALX School         #cisfun      ");
-	if (tab == NULL)
+	while (i <= len(str))
 	{
-		printf("Failed\n");
-		return (1);
+		if ((str[i] != ' ') && (str[i] != '\0'))
+		{
+			i++;
+		}
+		else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
+		{
+			words += 1;
+			i++;
+		}
+		else
+		{
+			i++;
+		}
 	}
-	print_tab(tab);
-	return (0);
+	return (words);
+}
+
+/**
+ * len - returns length of str
+ *@str: string to be counted
+ *
+ * Return: length of the string
+ */
+
+int len(char *str)
+{
+	int len = 0;
+
+	if (str != NULL)
+	{
+		while (str[len])
+			len++;
+	}
+	return (len);
 }
