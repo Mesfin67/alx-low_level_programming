21a75d262bb8b42c29ada5fe6fcc5c6756c3d993
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-23 21:18:41 -0700

    function that creates an array of integer

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,11 +3,6 @@
 /**
  * array_range - creates an array of integers from `min` to `max`, inclusive
  *
- * This function allocates memory for an array of integers, and initializes it
- * with the values from `min` to `max`, inclusive. If `min` is greater than
- * `max`, the function returns NULL. If the memory allocation fails, the
- * function also returns NULL.
- *
  * @min: smallest number in the array
  * @max: largest number in the array
  *
@@ -16,22 +11,26 @@
 
 int *array_range(int min, int max)
 {
-    int *array;
-    int i, j = 0;
+	int *array;
+	int i, j = 0;
 
-    if (min > max) {
-        return NULL;
-    }
+	if (min > max)
+	{
+		return (NULL);
+	}
 
-    array = malloc(sizeof(*array) * ((max - min) + 1));
-    if (array != NULL) {
-        for (i = min; i <= max; i++) {
-            array[j] = i;
-            j++;
-        }
-        return (array);
-    } else {
-        return (NULL);
-    }
+	array = malloc(sizeof(*array) * ((max - min) + 1));
+	if (array != NULL)
+	{
+		for (i = min; i <= max; i++)
+		{
+			array[j] = i;
+			j++;
+		}
+		return (array);
+	}
+	else
+	{
+		return (NULL);
+	}
 }
-
