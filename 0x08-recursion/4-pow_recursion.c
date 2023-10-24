5e775e14bf49bcb784fd241c35bd8bc6fbf49714
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-23 20:03:45 -0700

    pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,23 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of base raised to the power of exponent
+ * _pow_recursion - returns the value of base raised to the power of exponent
  * @base: the base
  * @exponent: the exponent
  *
  * Return: the value of base raised to the power of exponent
  */
-
 int _pow_recursion(int base, int exponent)
 {
 	if (exponent < 0)
+	{
 		return (-1);
+	}
 	else if (exponent == 0)
+	{
 		return (1);
+	}
 	return (_pow_recursion(base, exponent - 1) * base);
 }
+
+
