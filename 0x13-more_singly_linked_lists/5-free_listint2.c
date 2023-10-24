dcb30b1a561ef6209bc0c8fcff07efdc97981449
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-24 07:29:05 -0700

    free_listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,25 +1,21 @@
 #include "lists.h"
 
 /**
- *free_listint2 - frees a linked list
- *@head: pointer to the head of the list
- *Return: void
+ * free_listint - Free listint_t
+ *
+ * @head: Head of node
+ *
+ * Return: void
  */
 
-void free_listint2(listint_t **head)
+void free_listint(listint_t *head)
 {
-	listint_t *cursor;
-	listint_t **temp = head;
+	listint_t *new;
 
-	if (temp != NULL)
+	while (head)
 	{
-		while (*head != NULL)
-		{
-			cursor = *head;
-			free(cursor);
-			*head = (*head)->next;
-		}
-
-		*temp = NULL;
+		new = head;
+		head = head->next;
+		free(new);
 	}
 }
