2f45a0c8d975048216de1d82949f87d3d4ba0e45
Mesfin67 <mesfinbebulo@gmail.com>
2023-10-25 07:49:31 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,18 +5,7 @@
  */
 
 /*
-1. Declare a function that takes a string as an argument.
-2. Declare an integer variable to hold the length of the string.
-3. Declare an integer variable to hold the index of the string.
-4. Set the length variable to the length of the string.
-5. Set the index variable to the length of the string minus 1.
-6. Loop through the string starting at the last character and ending at the first character.
-7. Print the character at the current index.
-8. Decrement the index variable.
-9. Print a new line.
-*/
-/*
-Time Complexity: O(n)
+*Time Complexity: O(n)
 */
 void print_rev(char *s)
 {
@@ -37,16 +26,7 @@ void print_rev(char *s)
  */
 
 /*
-1. Declare a function that returns an integer and takes a pointer to a character as an argument.
-2. Declare an integer variable count and initialize it to 0.
-3. Declare an integer variable inc and initialize it to 0.
-4. Use a for loop to iterate through the string s.
-5. Increment inc by 1.
-6. Return inc.
-*/
-
-/*
-Time Complexity: O(n)
+*Time Complexity: O(n)
 */
 int _strlen(char *s)
 {
