93a86f4b65b7931f8f0df6a34a0c7e912d5dcec0
Mesfin67 <mesfinbebulo@gmail.com>
2023-11-23 05:35:36 -0800

    Add a function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
+/**
+ * print_number - Prints an integer.
+ * @n: Integer to be printed.
+ */
+void print_number(int n)
 {
-	int n;
-	int a[5];
-	int *p;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-	a[2] = 1024;
-	p = &n;
+	if (n / 10 != 0)
+		print_number(n / 10);
 
-	/* Set the value at the 6th index from the base address of p to 98. */
-	*(p + 5) = 98;
-
-	/* Prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	_putchar((n % 10) + '0');
 }
 
